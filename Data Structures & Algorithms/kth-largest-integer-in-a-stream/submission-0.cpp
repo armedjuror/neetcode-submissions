daa2b3c04deb82adfb3e1f71594f2cfca8ee710d
Armed Juror <ajwadjumanpc@gmail.com>
2026-09-04 11:44:07 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,24 @@
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (auto n: nums){
+            minHeap.push(n);
+            if (minHeap.size() > k){
+                minHeap.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+    
+private:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+};
