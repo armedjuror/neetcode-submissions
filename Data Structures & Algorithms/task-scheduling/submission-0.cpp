27cc3a380075f44d6bdd06465dedf5ddd6a9a5f9
Armed Juror <ajwadjumanpc@gmail.com>
2026-09-09 17:03:20 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> freq(26, 0);
+        for (auto t: tasks){
+            freq[t - 'A']++;
+        }
+        int maxFreq=0;
+        for (auto f: freq){
+            maxFreq = max(maxFreq, f);
+        }
+        int maxCount = 0;
+        for (int f:freq){
+            if (f==maxFreq)maxCount++;
+        }
+        return max((int)tasks.size(), (maxFreq-1)*(n+1)+maxCount);
+    }
+};
